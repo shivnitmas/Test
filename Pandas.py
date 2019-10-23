e64b6b4290cde556fc6916caf804237166db7afb
Susham Nandi <sushamnandi2468@gmail.com>
2019-10-24 02:47:30 +0530

    new file created

---
+++ Pandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+dict1={"name":['ajay','rishabh','amartya','shiv'],
+       "age":[12,23,36,33],
+       "location":['pune','bnglr','mumbai','chennai']}
+df=pd.DataFrame(dict1)
+print(df)
